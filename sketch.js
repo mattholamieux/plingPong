3739593b153319fc375b23fdc04018425073539a
mattholamieux <50525591+mattholamieux@users.noreply.github.com>
2019-08-08 16:14:00 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,180 @@
+// plingPong
+
+let spr;
+let blocks;
+let balls;
+let scl = 20;
+let synths = [];
+let walls;
+let notes = ["C2", "D2", "E2", "G2", "A3", "B3", "C3", "D3", "E3"];
+let reverb;
+let opacity;
+let open = true;
+
+function setup() {
+  createCanvas(1000, 600);
+  spr = createSprite(width / 2 - scl / 2, height / 2 - scl / 2, scl, scl);
+  spr.shapeColor = color(255);
+  balls = new Group();
+  blocks = new Group();
+  walls = new Group();
+  opacity = 90;
+
+  // blocks
+  for (i = 0; i < 10; i++) {
+    let bl1 = createSprite(([i] * 90 + 90), height / 1.3, scl * 4.4, scl);
+    let bl2 = createSprite(([i] * 90 + 90), height / 4.5, scl * 4.4, scl);
+    bl1.shapeColor = color(255, 90);
+    bl2.shapeColor = color(255, 90);
+    blocks.add(bl1);
+    blocks.add(bl2);
+  }
+
+  // walls
+  for (i = 0; i < 11; i++) {
+    let w = createSprite(([i] * 90 + 45), 0, 2, height * 2);
+    w.shapeColor = color(0, 0, 0, 0);
+    walls.add(w);
+  }
+
+  // synths
+  reverb = new p5.Reverb();
+  for (i = 0; i < 10; i++) {
+    synths[i] = new p5.MonoSynth();
+    reverb.process(synths[i], 3, 2);
+  }
+
+}
+
+function draw() {
+  background(50);
+  balls.collide(blocks, collision);
+  if (open) {
+    openScreen();
+  } else {
+      drawSprites();
+    sproutBalls();
+    removeBalls();
+    spr.displace(blocks);
+    balls.collide(blocks, collision);
+    blocks.collide(walls);
+    constraints();
+    nav();
+    touchStarted();
+    fade();
+    if (keyWentDown(32)) {
+      open = true;
+    }
+  }
+
+  function constraints() {
+    // constraints
+    for (i = 0; i < blocks.length; i++) {
+      blocks[i].position.y = constrain(blocks[i].position.y, scl, height - scl);
+    }
+    for (i = 0; i < balls.length; i++) {
+      if (balls[i].position.x > width - 50 || balls[i].position.x < 50) {
+        balls[i].velocity.x *= -1;
+      }
+    }
+
+  }
+
+
+  function sproutBalls() {
+    if (keyWentDown('z')) {
+      let b = createSprite(spr.position.x, spr.position.y, scl, scl);
+      b.velocity.y = scl / 4;
+      balls.add(b);
+    } else if (keyWentDown('x')) {
+      let b = createSprite(spr.position.x, spr.position.y, scl, scl);
+      b.velocity.y = scl / 8;
+      balls.add(b);
+    } else if (keyWentDown('c')) {
+      let b = createSprite(spr.position.x, spr.position.y, scl, scl);
+      b.velocity.y = scl / 16;
+      balls.add(b);
+    } else if (keyWentDown('v')) {
+      let b = createSprite(spr.position.x, spr.position.y, scl, scl);
+      b.velocity.y = scl / 32;
+      balls.add(b);
+    }
+  }
+
+  function removeBalls() {
+    if (keyIsDown(SHIFT)) {
+      for (i = 0; i < balls.length; i++) {
+        if (spr.overlap(balls[i])) {
+          balls[i].remove();
+        }
+      }
+    }
+  }
+
+  function collision(a, b) {
+    opacity = 255;
+    let dur = 1 / 8;
+    let v = random(0.2, 1);
+    let n = b.position.x / 90 - 1;
+    synths[n].play(notes[n], v, 0, dur);
+    a.velocity.y *= -1;
+  }
+
+  function fade() {
+    for (i = 0; i < blocks.length; i++) {
+      blocks[i].shapeColor = color(255, 180);
+      if (blocks[i].overlap(balls)) {
+        blocks[i].shapeColor = color(255, 255);
+      }
+    }
+  }
+
+  // function fade() {
+  //   // for (i=0;i<blocks.length;i++){
+  //   //   blocks[i].shapeColor = color(255,opacity);
+  //   // }
+  //   if (opacity > 90) {
+  //     opacity =opacity-1;
+  //   }
+  // }
+
+  function nav() {
+    if (keyIsDown(LEFT_ARROW)) {
+      spr.position.x -= scl / 4;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+      spr.position.x += scl / 4;
+    }
+    if (keyIsDown(UP_ARROW)) {
+      spr.position.y -= scl / 4;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+      spr.position.y += scl / 4;
+    }
+
+    spr.position.x = constrain(spr.position.x, scl, width - scl);
+    spr.position.y = constrain(spr.position.y, scl, height - scl);
+  }
+
+  function touchStarted() {
+    if (getAudioContext().state !== 'running') {
+      getAudioContext().resume();
+    }
+  }
+
+  function openScreen() {
+    let f = 100;
+    let directions = ['plingPong', 'arrows navigate', 'z,x,c,v deploy notes', 'shift deletes notes', 'spacebar toggles instructions', 'nudge paddles to change vertical position','click anywhere to begin'];
+    textSize(18);
+    textLeading(scl);
+    textFont('serif');
+    for (i = 0; i < directions.length; i++) {
+      fill(f);
+      text(directions[i], scl*3, scl * [i] + scl*15);
+      f += 15;
+    }
+    if (mouseIsPressed || keyWentDown(32)) {
+      open = false;
+    }
+  }
+}
